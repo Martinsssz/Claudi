f94644bb54af18ce2a17ba80fd392e8f16126dbc
edu78combr <eduardo.pdsilva04@gmail.com>
2024-09-21 15:53:35 -0300

    Eduardo 21/09/2024, Atualizando o sqlite

--- app/index.js
+++ app/index.js
@@ -3,19 +3,20 @@ import Signup from "./pages/Signup";
 import HomePage from "./pages/pagesWithHeader/HomePage";
 import { Redirect, useRouter, useFocusEffect } from "expo-router";
 import ResetPassword from "./pages/resetPassword";
-import { testar } from "./sqlite/dbService";
-
+import {mostrarUsuario} from "./sqlite/dbService"
 
 export default function App(){
-  testar()
   const router = useRouter()
 
-  useFocusEffect(()=>{
-    router.replace("pages/pagesWithHeader/HomePage")
-  }) 
-  return (
-    <Signup></Signup>
-    
-  )
+  let user = mostrarUsuario()
+  console.log(user)
+
+  if(user){
+    useFocusEffect(()=>{
+      router.replace("pages/pagesWithHeader/HomePage")
+    })
+  }else{
+    return <Signup></Signup>
+  }
   
 }
--- app/sqlite/dbService.js
+++ app/sqlite/dbService.js
@@ -1,47 +1,57 @@
-import * as SQLite from 'expo-sqlite';
+import * as SQLite from "expo-sqlite";
 
-const db = (async() => {
-  let database;
-  
-  (async () => {
-    database = await SQLite.openDatabaseAsync('claudi');
-  })();
-  
-  return database;
-})();
+let db;
 
-
-export async function testar(){
-  try{
-    await criarTabela()
-    //await atualizarTabelaUsuario()
-    await mostrarUsuários()
-  }catch(error){
-    console.log('Erro no teste:', error);
-  }
+async function abrirBanco() {
+  db = await SQLite.openDatabaseAsync("claudi");
 }
 
-export async function criarTabela(){
+export async function criarTabela() {
+  await abrirBanco();
   await db.execAsync(
-    `CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY NOT NULL,  username TEXT NOT NULL);`
-  )
+    `CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY NOT NULL,  username TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL);`
+  );
 }
 
-/*export async function atualizarTabelaUsuario(){
-  await db.runAsync(
-    `INSERT INTO user (id, username) VALUES (5486, "Dudu");`
-  )
-}*/
-
-export async function deletarDoBanco(id){
+export async function criarUsuario(user) {
+  //Void
+  await abrirBanco();
   const statement = await db.prepareAsync(
-    `DELETE FROM user WHERE id = $i`
-  )
-
-
+    `INSERT INTO user (id, username, email, password) VALUES ($i , $n, $e, $s);`
+  );
+  await statement.executeAsync({
+    $i: user.id,
+    $n: user.name,
+    $s: user.password,
+    $e: user.email,
+  });
 }
 
-export async function mostrarUsuários() {
-  let pessoas = await db.getAllAsync( `SELECT * from user;`)
+export async function atualizarTabelaUsuario(id, nome, email, senha) {
+  //Void
+  await abrirBanco();
+  const statement = await db.prepareAsync(
+    `UPDATE user SET  username = $n, email = $e, password = $s WHERE user_id = $i;`
+  );
+  await statement.executeAsync({
+    $i: id,
+    $n: nome,
+    $s: senha,
+    $e: email,
+  });
+}
+
+export async function deletarUsuario(id) {
+  //Void
+  await abrirBanco();
+  const statement = await db.prepareAsync(`DELETE FROM user WHERE id = $i`);
+  await statement.executeAsync({ $i: id });
+}
+
+export async function mostrarUsuario() {
+  //Array
+  await abrirBanco();
+  let pessoas = await db.getAllAsync(`SELECT * from user;`)
   console.log(pessoas)
+  return pessoas[0];
 }
